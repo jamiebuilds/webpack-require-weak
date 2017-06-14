963f21f2a13e77f6bafeed2011347a9c0b9c7ea5
James Kyle <me@thejameskyle.com>
2017-06-14 12:40:14 -0700

    Only require weak id when available to avoid messing up webpack internal state

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ const isWebpackBundle = require('is-webpack-bundle');
 
 /*::
 declare var __webpack_require__: Function;
+declare var __webpack_modules__: Object;
 */
 
-module.exports = isWebpackBundle ? __webpack_require__ : null;
+function requireWeak(id /*: number | string */ ) {
+  if (__webpack_modules__[id]) {
+    return __webpack_require__(id);
+  }
+}
+
+module.exports = isWebpackBundle ? requireWeak : null;
