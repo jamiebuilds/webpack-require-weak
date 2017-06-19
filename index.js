ab97b0176fc3fd58cc05632749acd79bc45d09bc
Sebastian Blade <blade254353074@hotmail.com>
2017-06-20 01:52:10 +0800

    Module for browser should use ES5 syntax. (#1)

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // @flow
 'use strict';
 
-const isWebpackBundle = require('is-webpack-bundle');
+var isWebpackBundle = require('is-webpack-bundle');
 
 /*::
 declare var __webpack_require__: Function;
